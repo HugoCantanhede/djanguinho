3592f3cbd2019c2a60b120b863e73436fd0de33a
hugocantanhede <hugo_cantanhede@hotmail.com>
2025-03-24 16:43:45 -0300

    criando classe usuario

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 
+from sistema import models
+
 # Register your models here.
 
+@admin.register(models.Usuário)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome' ,'email','ativo',)
+
+    
